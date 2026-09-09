e8b8307dd98ffae20db9d31fc66ba64f4ba8a403
jinkyborjaa-web <jinkyborjaa@gmail.com>
2026-09-09 15:54:28 +0800

    Delete server directory

--- server/role-migration.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users
-  ADD COLUMN role VARCHAR(20) NOT NULL DEFAULT 'user' AFTER password;
-
--- Run this separately for the account that should manage the system:
--- UPDATE users SET role = 'admin' WHERE username = 'your-admin-username';
